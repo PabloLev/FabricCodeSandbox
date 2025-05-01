ab1bb8cfe99ae7ced204612e4800776a155cddb6
Pablo Levin <pablevin@gmail.com>
2025-05-01 03:37:38 -0300

    Tailwind 4.1.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+    server: {
+        open: true,
+    },
+});
